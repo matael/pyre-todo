e5917a53a26d6fbf6356ec710ba1ec5ef9996d77
Mathieu (matael) Gaborit <mat.gaborit@gmx.com>
2012-06-01 10:29:57 +0200

    Add settings file

---
+++ settings.py
@@ -0,0 +1,19 @@
+# Settings for pyre-todo
+
+# Redis Host
+REDIS_HOST ="localhost"
+
+# Redis Port
+REDIS_PORT = 6379
+
+# Redis Database
+REDIS_DB = 2
+
+# Redis prefix for this app
+REDIS_PREFIX = "pyre:"
+
+# Path to static files
+STATIC_ROOT = "/home/matael/workspace/projects/pyre-todo/static"
+
+# Path to templates
+TEMPLATE_PATH = "/home/matael/workspace/projects/pyre-todo/templates"
